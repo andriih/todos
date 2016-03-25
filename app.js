3c43cf78acb56beceb23e2298fa3c75a22d768bc
Andrii Hnatyshyn <andrii.hnatyshyn@gmail.com>
2016-03-25 16:34:56 +0200

    delete add accounts

--- app.js
+++ app.js
@@ -5,20 +5,38 @@ if (Meteor.isClient) {
 
  Template.main.helpers({
     todos: function (){
-      return Todos.find();
+      return Todos.find({},{sort: {createdAt: -1}});
     }
  });
 
  Template.main.events({
     "submit .new-todo":function(event){
       var text = event.target.text.value;
-      console.log(text);
 
+      Todos.insert({
+        text: text,
+        createdAt: new Date()
+      });
+
+      //Clear the form
+
+      event.target.text.value='';
+
+      console.log(text);
+      //Prevent submit
       return false;
+    },
+    "click .toggle-checked":function(){
+      Todos.update(this._id,{$set:{checked: ! this.checked}});
+    },
+    "click .delete-todo": function(){
+      if(confirm("Are you sure ?")){
+      Todos.remove(this._id);
+      }
     }
  });
-}
 
-if (Meteor.isServer) {
- 
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+  });
 }
